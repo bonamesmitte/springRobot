0a1243f3ab23ca95e3b615d683c59a27869d1987
STUDENT_M16 <student_m16@PS-DAL-M16.local>
2020-09-07 00:53:03 -0500

    robot interface

---
+++ springProject/src/com/alice/entity/Robot.java
@@ -0,0 +1,9 @@
+package com.alice.entity;
+
+public interface Robot {
+	String attack();
+	String moveForward();
+	String moveBackward();
+	String turnRight();
+	String turnLeft();
+}
---
+++ springProject/src/com/alice/ui/RobotConsole.java
@@ -0,0 +1,9 @@
+package com.alice.ui;
+
+import com.alice.entity.Robot;
+
+public interface RobotConsole {
+	void print();
+
+	void setRobot(Robot robot);
+}
